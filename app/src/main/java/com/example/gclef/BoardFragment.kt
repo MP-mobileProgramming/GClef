163f4935a69fa1b0da3ca5a42142e36997491e47
EASYhz <jiton3@naver.com>
2022-05-11 13:16:02 +0900

    22.05.11

--- app/src/main/java/com/example/gclef/BoardFragment.kt
+++ app/src/main/java/com/example/gclef/BoardFragment.kt
@@ -1,9 +1,11 @@
 package com.example.gclef
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
@@ -11,7 +13,6 @@ import kotlinx.android.synthetic.main.fragment_board.*
 
 class BoardFragment : Fragment() {
     private var fireStore: FirebaseFirestore? = null
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +25,20 @@ class BoardFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
+        // recycler view
         recyclerView.adapter = RecyclerViewAdapter()
         recyclerView.layoutManager = LinearLayoutManager(context?.applicationContext)
 
+
+        // Create a post
+        fab.setOnClickListener {
+            val postIntent = Intent(context?.applicationContext, CreatePostActivity::class.java)
+            startActivity(postIntent)
+            Toast.makeText(context, "클릭", Toast.LENGTH_LONG).show()
+        }
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/gclef/CreatePostActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gclef
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+
+
+
+
+    }
+}
