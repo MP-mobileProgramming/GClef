27cf8a4e63a4aa453d87c4b90ecf9992307bd4b9
EASYhz <jiton3@naver.com>
2022-05-18 02:28:47 +0900

    22.05.18 (2)

--- app/src/main/java/com/example/gclef/CreatePostActivity.kt
+++ app/src/main/java/com/example/gclef/CreatePostActivity.kt
@@ -1,16 +1,35 @@
 package com.example.gclef
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_create_post.*
+import kotlinx.android.synthetic.main.list_item.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.jar.Manifest
+
 
 class CreatePostActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-
+    lateinit var storage: FirebaseStorage
+    lateinit var firestore: FirebaseFirestore
+    private var viewProfile : View? = null
+    var pickImageFromAlbum = 0
+    var selectImageUri : Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
@@ -21,9 +40,18 @@ class CreatePostActivity : AppCompatActivity() {
         var currentUser = auth.currentUser
 
 
+        storage = FirebaseStorage.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+
         //사진 업로드 코드 구현
         imageUploadButton.setOnClickListener {
             Toast.makeText(this, "사진 업로드", Toast.LENGTH_LONG).show()
+            var imagePickIntent = Intent(Intent.ACTION_PICK)
+            imagePickIntent.type = "image/*"
+            startActivityForResult(imagePickIntent, pickImageFromAlbum)
+
+
         }
 
 
@@ -33,29 +61,70 @@ class CreatePostActivity : AppCompatActivity() {
         }
 
         // 글쓰기 코드 구현
-        postButton.setOnClickListener {
-            var songPost = Song()
-            songPost.songTitle = songTitlePostEditText.text.toString()
-            songPost.songDetail = songDetailPostEditText.text.toString()
-            songPost.uid = currentUser?.uid
 
 
-
-            adapter.fireStore?.collection("Post")
-                ?.document()?.set(songPost)
-                ?.addOnSuccessListener {
-                    Toast.makeText(this, "업로드 완료", Toast.LENGTH_SHORT).show()
-                    finish()
-                }
-                ?.addOnFailureListener { exception ->
-                    // 실패할 경우
-                    Log.w("MainActivity", "Error getting documents: $exception")
-                }
-
-        }
 
 
 
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == pickImageFromAlbum) {
+            if (resultCode == Activity.RESULT_OK) {
+                selectImageUri = data?.data
+                ImageUpload(this)
+
+            }
+
+        }
+    }
+
+    private fun ImageUpload(view: CreatePostActivity) {
+        var timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imgFileName = "IMAGE_" + timeStamp + "_png"
+        var storageRef = storage?.reference?.child("images")?.child(imgFileName)
+
+        storageRef?.putFile(selectImageUri!!)?.addOnSuccessListener {
+            Toast.makeText(this, "이미지 업로드 완료", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.post_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val adapter = RecyclerViewAdapter()
+        auth = FirebaseAuth.getInstance()
+        var currentUser = auth.currentUser
+
+        when(item.itemId) {
+            R.id.postButton-> {
+                var songPost = Song()
+                songPost.songTitle = songTitlePostEditText.text.toString()
+                songPost.songDetail = songDetailPostEditText.text.toString()
+                songPost.uid = currentUser?.uid
+
+
+
+                adapter.fireStore?.collection("Post")
+                    ?.document()?.set(songPost)
+                    ?.addOnSuccessListener {
+                        Toast.makeText(this, "업로드 완료", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                    ?.addOnFailureListener { exception ->
+                        // 실패할 경우
+                        Log.w("MainActivity", "Error getting documents: $exception")
+                    }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
