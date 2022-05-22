a4b02563256bcf699d2adf719df19e423c7c687e
sooin shin <sooin9440@gmail.com>
2022-05-23 00:58:40 +0900

    음악 끝나면 다시 재생버튼으로 바뀌고 맨 처음으로 가야하는데 안바뀜 -> 수정해야해

--- app/src/main/java/com/example/gclef/InPostActivity.kt
+++ app/src/main/java/com/example/gclef/InPostActivity.kt
@@ -5,7 +5,9 @@ import android.media.MediaPlayer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.SeekBar
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.signin.internal.Storage
@@ -17,6 +19,9 @@ import kotlinx.android.synthetic.main.activity_in_post.*
 class InPostActivity : AppCompatActivity()  {
     private lateinit var data : Song
     private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var seekBar1: SeekBar
+    lateinit var runnable: Runnable
+    private var handler=Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,19 +44,47 @@ class InPostActivity : AppCompatActivity()  {
         mediaPlayer.prepare()
         playButton.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24)
 
+        //seekBar initialize
+        seekBar1=findViewById(R.id.seekBarInPost);
+        seekBar1.progress=0
+        seekBar1.max=mediaPlayer.duration
 
        playButton.setOnClickListener {
             if(mediaPlayer.isPlaying) {
-                Toast.makeText(this, "재생 중", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "재생 중", Toast.LENGTH_SHORT).show()
                 playButton.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24)
                 mediaPlayer.pause()
-
             }
             else {
-                Toast.makeText(this, "재생 시작", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "재생 시작", Toast.LENGTH_SHORT).show()
                 mediaPlayer.start()
                 playButton.setBackgroundResource(R.drawable.ic_baseline_pause_24)
             }
+        }
+
+        seekBar1.setOnSeekBarChangeListener(object:SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean) {
+                //when the position of seekbar has changed
+                if(changed){
+                    mediaPlayer.seekTo(pos)
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
+        runnable=Runnable{
+            seekBar1.progress=mediaPlayer.currentPosition
+            handler.postDelayed(runnable,1000)
+        }
+        handler.postDelayed(runnable,1000)
+        //when music is finished, back to 0
+        mediaPlayer.setOnCompletionListener {
+            playButton.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24)
+            seekBar1.progress=0
         }
 
     }
